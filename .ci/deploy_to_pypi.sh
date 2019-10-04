cc41c97c982ea8fe3c372c5d5d835bba99527714
Roberto Di Remigio <roberto.diremigio@gmail.com>
2019-10-04 16:06:18 +0200

    Almost ready to try deployment

--- .ci/deploy_to_pypi.sh
+++ .ci/deploy_to_pypi.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-docker run --rm -v $(pwd):/io \
+docker run \
+       --env MATURIN_PASSWORD="$MATURIN_PASSWORD" \
+       --rm \
+       -v "$(pwd)":/io \
        konstin2/maturin publish \
+       --python python3.6 python3.7 \
        --username robertodr \
        --password "$MATURIN_PASSWORD"
