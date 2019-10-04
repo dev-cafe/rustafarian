8a2c3f24b5e29640e1ed1560a3a4489b31757748
Roberto Di Remigio <roberto.diremigio@gmail.com>
2019-10-04 16:34:32 +0200

    Bump to version 0.3.12

--- .ci/deploy_to_pypi.sh
+++ .ci/deploy_to_pypi.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+# Use https://hub.docker.com/r/konstin2/maturin to deploy
+
 docker run \
        --env MATURIN_PASSWORD="$MATURIN_PASSWORD" \
        --rm \
        -v "$(pwd)":/io \
-       konstin2/maturin publish \
+       konstin2/maturin:master \
+       publish \
        --python python3.6 python3.7 \
        --username robertodr \
        --password "$MATURIN_PASSWORD"
