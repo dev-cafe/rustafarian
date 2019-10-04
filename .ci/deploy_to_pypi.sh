c921f7a74a6dbe8c917841f819e9a06b86d817f1
Roberto Di Remigio <roberto.diremigio@gmail.com>
2019-10-04 20:02:45 +0200

    Fix Docker image

--- .ci/deploy_to_pypi.sh
+++ .ci/deploy_to_pypi.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# Use https://hub.docker.com/r/konstin2/maturin to deploy
+# Use https://hub.docker.com/r/robertodr/maturin to deploy
 
 docker run \
        --env MATURIN_PASSWORD="$MATURIN_PASSWORD" \
        --rm \
        -v "$(pwd)":/io \
-       konstin2/maturin:master \
+       robertodr/maturin \
        publish \
        --interpreter python3.6 python3.7 \
        --username robertodr \
